4c6821ddf31c99197f6db5eecf27a4efb67535a4
tobo36 <63086864+tobo36@users.noreply.github.com>
2020-09-06 14:44:41 +1000

    Update UserRace.kt

--- src/main/kotlin/world/UserRace.kt
+++ src/main/kotlin/world/UserRace.kt
@@ -3,3 +3,28 @@ package world
 enum class UserRace {
     Human
 }
+
+enum class UserRace {
+    Elf
+}
+
+enum class UserRace {
+    Dwarf
+}
+
+enum class UserRace {
+    Orc
+}
+
+enum class UserRace {
+    Demon Born
+}
+
+enum class UserRace {
+    Angelic 
+}
+
+enum class UserRace {
+    Hobbit
+}
+
